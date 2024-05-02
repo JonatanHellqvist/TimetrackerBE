c9a4c734d25058f5853acdff8df5d11217e683a1
JonatanHellqvist <143435050+JonatanHellqvist@users.noreply.github.com>
2024-05-02 11:15:09 +0200

    Docker

--- src/main/java/com/timetrackerbe/timetrackerbe/controllers/ActivityController.java
+++ src/main/java/com/timetrackerbe/timetrackerbe/controllers/ActivityController.java
@@ -2,6 +2,7 @@ package com.timetrackerbe.timetrackerbe.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.timetrackerbe.timetrackerbe.models.Activity;
 import com.timetrackerbe.timetrackerbe.services.ActivityService;
 
+
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class ActivityController {
 
 	private ActivityService activityService;
