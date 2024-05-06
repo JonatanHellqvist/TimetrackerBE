5ddf92070ef85b99a175183f82eaf483ae0e1418
JonatanHellqvist <143435050+JonatanHellqvist@users.noreply.github.com>
2024-05-06 10:16:20 +0200

    User mappings

---
+++ src/main/java/com/timetrackerbe/timetrackerbe/controllers/UserController.java
@@ -0,0 +1,39 @@
+package com.timetrackerbe.timetrackerbe.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.timetrackerbe.timetrackerbe.models.User;
+import com.timetrackerbe.timetrackerbe.services.UserService;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class UserController {
+	
+	private UserService userService;
+
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@PostMapping("/user")
+	public User addUser(@RequestBody User user) {
+		return userService.addUser(user);
+	}
+
+	@GetMapping("/user/{id}")
+	public User getUser(@PathVariable String id) {
+		return userService.getUser(id);
+	}
+
+	@GetMapping("/activities")
+	public List<User> getUsers() {
+		return userService.getUsers();
+	}
+}
---
+++ src/main/java/com/timetrackerbe/timetrackerbe/services/UserService.java
@@ -0,0 +1,39 @@
+package com.timetrackerbe.timetrackerbe.services;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+
+import com.timetrackerbe.timetrackerbe.models.User;
+
+@Service
+public class UserService {
+	private final MongoOperations mongoOperations;
+
+	public UserService(MongoOperations mongoOperations) {
+		this.mongoOperations = mongoOperations;
+	}
+	
+	public List<User> getUsers() {
+		return mongoOperations.findAll(User.class);
+	}
+
+	public User getUser(String id) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("id").is(id));
+		
+		return mongoOperations.findOne(query, User.class);
+	}
+	
+	public User addUser(User user) {
+		return mongoOperations.insert(user);
+	}
+
+	public void deleteUser(String id) {
+		Query query = Query.query(Criteria.where("id").is(id));
+		mongoOperations.remove(query, User.class);
+	}
+}
